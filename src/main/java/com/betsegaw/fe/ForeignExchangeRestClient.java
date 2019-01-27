91b3c32146dd48422e4c497ecedd9f089d5aa009
Betsegaw Lemma <betsegawlemma@gmail.com>
2019-01-28 01:59:43 +0300

    Correct dependency

--- src/main/java/com/betsegaw/fe/ForeignExchangeRestClient.java
+++ src/main/java/com/betsegaw/fe/ForeignExchangeRestClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 @Service
 public class ForeignExchangeRestClient {
 	
-	private final String API_ACCESS_KEY = "4f2f2232957061cd29e84bb11e2ff5f9";
+	private final String API_ACCESS_KEY = "c78d1728a68ff4e023217d10f6a3dd3e";
 
 	private RestTemplate restTemplate;
 
